9c1b84c48bb433e352b87c9ebded8f66333c8f5e
Mahmood Bagheri <createdbymahmood@gmail.com>
2020-12-22 13:54:46 +0330

    style: :art: avoid arrowparens and format the app

--- packages/application/src/App.tsx
+++ packages/application/src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from '@monorepo/components';
 import { useStore } from '@monorepo/global';
 
 function App() {
-    const bears = useStore((state) => state.bears);
+    const bears = useStore(state => state.bears);
     return (
         <div>
             {JSON.stringify(bears)}
